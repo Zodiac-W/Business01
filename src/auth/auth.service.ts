2b43367be8efc6f0ecc00141f0bc641fbcbd1d72
Zodiac-W <thegame11m@gmail.com>
2023-07-04 12:54:41 +0300

    Fixed auth module error hadnling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user-dto';
 import { LoginUserDto } from 'src/users/dto/login-user-dto';
@@ -12,31 +12,43 @@ export class AuthService {
   ) {}
 
   async signupUser(createUserDto: CreateUserDto): Promise<any> {
-    const user = await this.usersService.signupUser(createUserDto);
-    if (user.message) {
-      return user;
-    }
-    const payload = { sub: user.id };
+    try {
+      const user = await this.usersService.signupUser(createUserDto);
+      if (user.message) {
+        return user;
+      }
+      const payload = { sub: user.id };
 
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.NOT_FOUND);
+    }
   }
 
   async loginUser(loginUserDto: LoginUserDto): Promise<any> {
-    const user = await this.usersService.loginUser(loginUserDto);
-    if (user.message) {
-      return user;
-    }
-    const payload = { sub: user.id };
+    try {
+      const user = await this.usersService.loginUser(loginUserDto);
+      if (user.message) {
+        return user;
+      }
+      const payload = { sub: user.id };
 
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.NOT_FOUND);
+    }
   }
 
   async validateUsers(payload: any): Promise<any> {
-    const user = await this.usersService.validateUser(payload.sub);
-    return user;
+    try {
+      const user = await this.usersService.validateUser(payload.sub);
+      return user;
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.NOT_FOUND);
+    }
   }
 }
