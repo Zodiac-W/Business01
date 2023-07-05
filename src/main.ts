75399595befdf53cf8cd0a7f3f3f27830da80acc
Zodiac-W <thegame11m@gmail.com>
2023-07-05 10:27:19 +0300

    Added to swagger how to set auth token

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+
+  const commonDescription =
+    'Non public entry points are protected by JWT token. Valid token must be provided in header "Authorization". Example:\n\n' +
+    '> Authorization: Bearer *TOKEN*';
+
   const config = new DocumentBuilder()
     .setTitle('Business01')
-    .setDescription('LMS application for Business01')
+    .setDescription(
+      `LMS application for Business01 ` + ' \n' + commonDescription,
+    )
     .setVersion('0.1.0-Alpha')
     .addTag('All the business content you will ever need')
     .build();
